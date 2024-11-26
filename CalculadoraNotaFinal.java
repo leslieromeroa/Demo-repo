7a1199b53e108107dadf76571e2b5de6dd38b235
Leslie Romero <leslie.romero@alumnos.uneatlantico.es>
2024-11-26 12:52:42 +0100

    CalculadoraNotaFinal.java

--- CalculadoraNotaFinal.java
+++ CalculadoraNotaFinal.java
@@ -23,30 +23,4 @@ class CalculadoraNotaFinal {
 
     System.out.printIn ("Nota final = " + notaFinal);            
 
-    class HolaMundo {
-    public static void main(String[] args) {
-
-        double dinero;
-        String nombre;
-        char unChar;
-
-        edad= 18;
-        dinero = 100;
-        nombre = "leslie";
-        unChar = "x";
-
-        System.out.printIn (edad);
-        System.out.printIn (dinero);
-        System.out.printIn (nombre);
-
-        System.out.printIn("Me llamo" + nombre + ", tengo" + edad "años y " + dinero + "euros")
-
-        dinero = dinero + 100;
-
-        System.out.printIn("Ahora tengo " + dinero + "euros");
-
-        dinero = dinero - 50;
-         
-        System.out.printIn("Ahora tengo " + dinero + "euros");
-
 
